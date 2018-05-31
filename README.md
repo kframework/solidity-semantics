768a1b5489087800d56d2349b62ec1b6c4fb42b3
Daniele Filaretti <dfilaretti@gmail.com>
2018-05-31 20:57:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# solidity-semantics
+
+A K semantics of the Solidity programming language.
+More info here...
+
+## Intro 
+
+## How to build/run 
+
+## Etc...
+
+
---
+++ tests/SimpleStorage.sol
@@ -0,0 +1,12 @@
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) {
+        storedData = x;
+    }
+
+    function get() constant returns (uint retVal) {
+        return storedData;
+    }
+    
+}
