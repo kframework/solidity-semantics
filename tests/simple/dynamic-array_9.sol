95529bdafd8d9e246015d74ef530c8451dca60f8
Shang-Wei LIN <shangweilin@gmail.com>
2019-08-29 01:19:36 +0800

    Add one more test case

---
+++ tests/simple/dynamic-array_9.sol
@@ -0,0 +1,81 @@
+
+contract Test {
+    uint[][2] array;
+
+    function arrayPush(uint index, uint value) public {
+        array[index].push(value);
+    }
+
+    function arrayPop(uint index) public returns (uint) {
+        uint len = array[index].length;
+        uint result = array[index][len - 1];
+        array[index].pop();
+
+        return result;
+    }
+
+    function getArrayLength(uint index) public returns (uint) {
+        return array[index].length;
+    }
+}
+
+
+{
+
+"Test1" : {
+    "exec" : {
+        "code" : #solidity(
+                    Test t = new Test();
+                    t.arrayPush(0,100);
+                    uint len = t.getArrayLength(0);
+                    int value = t.arrayPop(0);
+                    uint len2 = t.getArrayLength(1);
+                )
+    },
+    "post" : {
+        "mem" :  #exists("len", 1),
+        "mem" :  #exists("value", 100),
+        "mem" :  #exists("len2", 0)
+    }
+},
+
+"Test2" : {
+    "exec" : {
+        "code" : #solidity(
+                    Test t = new Test();
+                    t.arrayPush(1,100);
+                    uint len1 = t.getArrayLength(1);
+                    t.arrayPush(1,200);
+                    uint len2 = t.getArrayLength(1);
+                    t.arrayPush(1,300);
+                    uint len3 = t.getArrayLength(1);
+
+                    t.arrayPush(0,10);
+                    uint len4 = t.getArrayLength(0);
+                    t.arrayPush(0,20);
+                    uint len5 = t.getArrayLength(0);
+
+                    uint value1 = t.arrayPop(1);
+                    uint value2 = t.arrayPop(1);
+                    uint value3 = t.arrayPop(1);
+
+                    uint len6 = t.getArrayLength(1);
+                    uint len7 = t.getArrayLength(0);
+                )
+    },
+    "post" : {
+        "mem" :  #exists("len1", 1),
+        "mem" :  #exists("len2", 2),
+        "mem" :  #exists("len3", 3),
+        "mem" :  #exists("len4", 1),
+        "mem" :  #exists("len5", 2),
+        "mem" :  #exists("len6", 0),
+        "mem" :  #exists("len7", 2),
+        "mem" :  #exists("value1", 300),
+        "mem" :  #exists("value2", 200),
+        "mem" :  #exists("value3", 100)
+    }
+}
+
+
+}
